c66ba95668a9130153cafd31ac8dafb0d4e15916
SnappeR4 <rahulwaghmare751@gmail.com>
2025-03-31 13:13:00 +0530

    Get orders by the phone

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,7 @@ const OrderController = require('../controllers/OrderController');
 
 r̥outer.post('/create', OrderController.createOrder); // Create Order
 r̥outer.get('/all', OrderController.getOrders); // Get All Orders
+r̥outer.get('/getOrdersByPhone', OrderController.getOrdersByPhone)
 r̥outer.get('/getById', OrderController.getOrderById); // Get Order by ID
 r̥outer.put('/update-status', OrderController.updateOrderStatus); // Update Order Status
 r̥outer.delete('/delete', OrderController.deleteOrder); // Delete Order
